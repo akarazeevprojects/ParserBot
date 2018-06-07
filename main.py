7d565aec9e5892240479d6184a6e9285debbb502
Anton Karazeev <anton.karazeev@gmail.com>
2018-06-07 21:32:07 +0300

    Fix some errors

--- main.py
+++ main.py
@@ -1,36 +1,28 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from emoji import emojize
 import numpy as np
-import os
-import pickle
 import threading
 import argparse
 import telegram
 import sys
-import logging
+import os
+import pickle
 import utils
 import json
 import time
+from emoji import emojize
 
 # sys.argv = ['--proxy 1']
+pause = 10.0
 
 if os.path.exists(utils.PKL) is False:
-        data = list()
-        with open(utils.PKL, 'wb') as f:
-            pickle.dump(data, f)
+    data = list()
+    with open(utils.PKL, 'wb') as f:
+        pickle.dump(data, f)
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - \
-                            %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 parser = argparse.ArgumentParser(description="Bot to parse news from FPMI's site")
 parser.add_argument('--proxy', dest='proxy', type=int,
                     default=1, help='use proxy or not')
-
-
-pause = 10.0
 
 
 class Looper(threading.Thread):
@@ -48,12 +40,10 @@ class Looper(threading.Thread):
     def stop(self):
         self.stop_event.set()
 
-
 bot = None
 info_text = 'NOT IMPLEMENTED'
 fpmi_url = 'https://mipt.ru/education/departments/fpmi/'
 channel_id = -1001180214136  # FPMI_announcements.
-
 
 def announce():
     news_list = utils.get_info(fpmi_url)
@@ -87,21 +77,21 @@ def help(bot, update):
 
 
 announcer = Looper(announce, pause=pause)  # Seconds.
-
-
 def main():
     global bot
     token = utils.get_token('res/token.json')
 
     args = parser.parse_args()
     print(args.proxy)
-
+    print(type(args.proxy))
     if args.proxy == 1:
+        print('-> USE PROXY')
         req = telegram.utils.request.Request(proxy_url='socks5://127.0.0.1:9050',
                                              read_timeout=30, connect_timeout=20,
                                              con_pool_size=10)
         bot = telegram.Bot(token=token, request=req)
     elif args.proxy == 0:
+        print('-> NO PROXY')
         bot = telegram.Bot(token=token)
     else:
         raise ValueError('Wrong proxy.')
